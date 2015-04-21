cdbe100f4c093c39f1ad9d0e4d6c4399f8218457
McNeillJames <james.mcneill@my.jcu.edu.au>
2015-04-21 21:37:01 +1000

    fixing some indenting issues

--- Plan/site/artistList.php
+++ Plan/site/artistList.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <meta charset="UTF-8">
-    <title></title>
+	<meta charset="UTF-8">
+	<title></title>
 </head>
 <body>
 
@@ -11,7 +11,7 @@
 
 	$artists = $dbh->query("SELECT * FROM Artist");
 
-    //write summary of every artist retrieved in $artists
+	//write summary of every artist retrieved in $artists
 	foreach ($artists as $row) {
 		echo "Artist Title: " . $row["A_Title"] . "<br/>";
 		echo "Contact Info:\n<ul>\n<li>Email: " . $row["A_Email"] . 
@@ -25,24 +25,24 @@
 
 		$all_images = $dbh->query("SELECT Img_Ref FROM Images WHERE Img_Id IN (
 									SELECT ImgId FROM ArtistImage WHERE A_Id = " . $artist_id . ")");
-        // there's only one row in $main_image
-        $main_image_row = $main_image->fetch(PDO::FETCH_ASSOC);
+		// there's only one row in $main_image
+		$main_image_row = $main_image->fetch(PDO::FETCH_ASSOC);
 
-        // check if this artist has a main image assigned
-        if(!is_null($main_image_row["Img_Ref"]))
-        {
-            echo "<a href='artistDetails.php'><img src='../db/" . $main_image_row["Img_Ref"] . "'></a><br/>";
-        }
-        // use f'n something if there isn't one assigned
+		// check if this artist has a main image assigned
+		if(!is_null($main_image_row["Img_Ref"]))
+		{
+			echo "<a href='artistDetails.php'><img src='../db/" . $main_image_row["Img_Ref"] . "'></a><br/>";
+		}
+		// use f'n something if there isn't one assigned
 		else if(count($all_images) > 1)
 		{
 			$chosen_row = $all_images->fetch(PDO::FETCH_ASSOC);
 			echo "<a href='artistDetails.php'><img src='../db/" . $chosen_row["Img_Ref"] . "'></a><br/>";
 		}
 		else
-        {
-        	echo "<p>No image found</p>";
-        }
+		{
+			echo "<p>No image found</p>";
+		}
 	}
 
 ?>
