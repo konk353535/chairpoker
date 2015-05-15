25eaef4cb04e87813c7a414022638116fc1f1165
McNeillJames <james.mcneill@my.jcu.edu.au>
2015-05-15 18:58:57 +1000

    putting nav include here, not using it yet

---
+++ website/include/inc_nav.php
@@ -0,0 +1,13 @@
+<div id="nav">
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="events.php">Events</a></li>
+  <li><a href="bulletin.php">Bulletin</a></li>
+  <li><a href="artists.php">Artists</a></li>
+  <li><a href="signUp.php">Sign Up</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="userProfile.php">My Profile</a></li>
+  <li><form><input type="search" class="navSearch" placeholder="Search">
+  </form></li>
+</ul>
+</div>
