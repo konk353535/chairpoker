2f63de2ec02423b4cd9b443b09eecb74e7368bf1
McNeillJames <james.mcneill@my.jcu.edu.au>
2015-05-15 14:37:25 +1000

    removed some var_dump calls

--- website/addMember.php
+++ website/addMember.php
@@ -1,6 +1,5 @@
 <?php
 	include("include/db_connect.php");
-	var_dump($_POST);
     if(!isset($_POST['signUpForm'])){
         // probably here by accident, let's go somewhere more sensible
         header("Location: index.html");
@@ -19,15 +18,14 @@
                                     ":Fname" => $_POST['Fname'],
                                     ":Sname" => $_POST['Sname'],
                                     ":AuthLevelId" => 1));
-    var_dump($new_member_statement->rowCount());
     if($new_member_statement->rowCount() > 0) {
         // successfully signed up, automatically sign in and direct to profile
         session_start();
-        $_SESSION['Id'] = $dbh->lastInsertId();
+        session_regenerate_id();
+        $_SESSION['Member_Id'] = $dbh->lastInsertId();
         $_SESSION['Email'] = $_POST['Email'];
         $_SESSION['Name'] = $_POST['Fname'] . " " . $_POST['Sname'];
         
-        session_regenerate_id();
        	header("Location: userProfile.php");
         exit();
     }
