aa648bf675fa5e347b37cc73eae4108b24d411a6
McNeillJames <james.mcneill@my.jcu.edu.au>
2015-05-19 13:15:47 +1000

    implemented generating login form based on login status

--- website/template.php
+++ website/template.php
@@ -1,3 +1,6 @@
+<?php session_start(); 
+$uri = $_SERVER['REQUEST_URI'];
+?>
 <!doctype html>
 <html>
 <head>
@@ -10,14 +13,36 @@
 
 <div id="wrapper">
     <div id="header">
-        <div class="loginForm">
-            <form id="form1" name="form1" method="post">
-                <input name="textfield" type="text" id="textfield" placeholder="Username" class="input"><br />
+		<?php
+			//check if logged in
+			if(isset($_SESSION['Member_Id'])){
+		?>
+		<!-- unsure how to style this -->
+		<div class="loginForm">
+			<form id="form1" name="form1" method="post" action="login/logout.php?redirect=<?php echo $uri; ?>">
+				<a href="userProfile.php">My Profile</a>
+				<input type="submit" name="logout" value="Log Out" class="input"><br />
+			</form>
+		</div>
+		<?php
+			}
+			// if not logged in
+			else {
+		?>
+		<div class="loginForm">
+            <form id="form1" name="form1" method="post" action="login/loginAuth.php?redirect=../userProfile.php&redirectFailure=<?php echo $uri; ?>">
+                <input name="Email" type="text" id="textfield" placeholder="Email" class="input"><br />
                 <input name="password" type="password" id="password" placeholder="********" class="input"><br />
-                <input type="button" name="button" id="button" value="Button" class="input"><br />
+                <input type="submit" name="loginForm" id="button" value="Log In" class="input"><br />
                 <a href="signUp.html">Not a Member? </a>
             </form>
-        </div>
+        </div>	
+		<?php
+			}
+			if(isset($_SESSION['errMsg'])){
+				echo "<p>" . $_SESSION['errMsg'] . "</p>";	
+			}
+		?>
         <div class="headerImage">
             <a href="index.html">
                 <img src="images/TCMC150100.jpg" width="150" height="100" alt="Townsville Community Music Centre"/>
