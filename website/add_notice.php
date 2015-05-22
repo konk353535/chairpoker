d92b6827e37212ae6d86a666f3aeacfcf64ca680
konk353535 <konk353535@hotmail.com>
2015-05-22 18:03:02 +1000

    Jason changes to notice page and css

--- website/add_notice.php
+++ website/add_notice.php
@@ -41,6 +41,9 @@
 		// Connect to our database
 		include("../include/db_connect.php");
 
+		// Start Session for this page
+		session_start();
+
 		// Check that user is logged in (only registered users can make notices)
 		$auth_level = $_SESSION['AuthLevel'];
 
@@ -59,19 +62,14 @@
     <!-- Main Content Here -->
       <h1>New Notice</h1>
    	<form name="loginForm" method="post" action="notices.php?action=new_notice" onsubmit="return input_validate_notice()">
-   		<table width="100%">
-   			<tr>
-   				<td width="50%">Notice Expiry Date</td>
-   				<td width="50%"><input name="notice_expiry_date" type="text"  placeholder="Expiry Date" id="expiry_date"></td>
-			</tr>
-			<tr>
-				<td>Notice Description</td>
-				<td><textarea rows="10" cols="50" name="notice_description" id="notice_description"></textarea></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" name="button" value="Submit" >
-		</table>
+
+		<label class="label">Notice Expiry Date</label>
+		<input class="full_width" name="notice_expiry_date" type="text"  placeholder="Expiry Date" id="expiry_date">
+
+		<label class="label">Notice Description</label>
+		<textarea class="full_width no_resize" name="notice_description" id="notice_description" rows="10"></textarea>
+
+		<input type="submit" name="button" value="Submit" >
 	</form>
 	<div id="errorOutput"></div>
     <!-- End of Main Content -->
