6c58fb33f07e54e883e209aa3912add42581622f
konk353535 <konk353535@hotmail.com>
2015-05-22 18:13:18 +1000

    Index page with proper templating

---
+++ website/index.php
@@ -0,0 +1,55 @@
+
+
+
+
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Townsville Community Music Centre</title>
+	<link href="mainstyle.css" rel="stylesheet" type="text/css">
+
+	<!-- Google Fonts Open Sans -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<!-- Logo + Login Form + Text Title -->
+<?php include('include/header.php'); ?>
+<!-- End of header (Logo/LoginForm/TextTitle) -->
+
+<!-- Nav Bar -->
+<?php include('include/inc_nav.php'); ?>
+
+<!-- All Content -->
+<div class="allContent">
+    <div class="mainContent bgPrimary">
+
+    <h1>Home Page</h1>
+    <p>This is the home page peps!</p>
+    <!-- End of Main Content -->
+
+    </div>
+    <div class="sideContent bgPrimary">
+        <h1>Hello</h1>
+        <p>Test</p>
+    </div>
+
+</div>
+<!-- End of all Content -->
+
+<!-- Footer Template -->
+
+<div class="wrapper bgPrimary">
+    <div class="footer">
+        Footer Content 
+    </div>
+</div>
+
+<!-- End Footer Template -->
+
+</body>
+</html>
