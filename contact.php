48d7b52f87e318e55f2290f2907f2f402bfbf5f3
khabbott12 <kate.abbott@my.jcu.edu.au>
2015-05-26 01:52:30 +1000

    contact.php

---
+++ contact.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Townsville Community Music Centre</title>
+	<link href="mainstyle.css" rel="stylesheet" type="text/css">
+
+	<!-- Google Fonts Open Sans -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<!-- Logo + Login Form + Text Title -->
+<?php include('template/header.php'); ?>
+<!-- End of header (Logo/LoginForm/TextTitle) -->
+
+<!-- Nav Bar -->
+<?php include('template/inc_nav.php'); ?>
+
+<!-- All Content -->
+<div class="allContent bgPrimary">
+    
+    
+	<h1 id= "contact_tagline">Contact Us!</h1>
+    <h2 class= "contact_undertagline"> Have questions? Here's how to reach us!
+    <br> Office Hours : 9:30-2:30 Mon-Wed</h2>
+    
+    
+<p>Townsville Civic Theatre :PO Box1006 <br>
+<a href="https://www.google.com.au/maps/place/Townsville+Civic+Theatre/@-19.271564,146.809208,17z/data=!3m1!4b1!4m2!3m1!1s0x6bd5f92267e9596d:0x58cd8e35887d45e0?hl=en" target="_blank">
+    41 Boundary St.
+    <br>
+    Townsville, QLD 4810</a>
+    <br>
+    Telephone: 07 4724 2086
+    <br>
+    Mobile Phone(For non-office hours):0402 255 182
+    <br>
+    E-mail:<a href="admin@townsvillemusic.org.au" target="_blank"> admin@townsvillemusic.org.au</a>
+    </p>
+    <hr>
+    <a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534" alt="facebook icon" target="_blank">
+    	<img src="images/FacebookIcon.jpg" align="center"></a>
+    <h2> Thank you to our wonderful sponsors who give us so much support!</h2>
+    <p>Our music concerts are well-publicised and well-attended and sponsors enjoy brand exposure throughout the Arts community. We welcome sponsorship or assistance in any form.</p>
+    <hr>
+    <h3> Townsville City Council</h3>
+    <p>The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. 
+The Council also assists with the performance venues for our concerts and workshops.</p>
+<hr>
+<h3> Queensland Government</h3>
+<p>he Gambling Community Benefit Fund has assisted us to obtain office equipment and sound and lighting equipment for our product </p>
+<hr>
+    
+    
+                        
+    
+    </div>
+
+<!-- End of all Content -->
+
+<!-- Footer Template -->
+<?php include('template/footer.php'); ?>
+<!-- End Footer Template -->
+
+</body>
+</html>
