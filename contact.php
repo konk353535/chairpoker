62567ee42a09d1aa97959e4dff5d6eaeeb94225e
konk353535 <konk353535@hotmail.com>
2015-05-27 11:27:47 +1000

    Changes to contact page

--- contact.php
+++ contact.php
@@ -1,11 +1,4 @@
-<<<<<<< Updated upstream
-=======
 
-
-
-
-
->>>>>>> Stashed changes
 <!doctype html>
 <html>
 <head>
@@ -29,15 +22,15 @@
 
 <!-- All Content -->
 <div class="allContent bgPrimary">
-    
-    
-	<h1 class="contact_tagline"> Contact Us!</h1>
+  <div class="mainContent bgPrimary">
+
+    <h1 class="contact_tagline"> Contact Us!</h1>
     <h2 class= "contact_undertagline"> Have questions? Here's how to reach us!
     <br> Office Hours : 9:30-2:30 Mon-Wed</h2>
-    
-    
-<p>Townsville Civic Theatre :PO Box1006 <br>
-<a href="https://www.google.com.au/maps/place/Townsville+Civic+Theatre/@-19.271564,146.809208,17z/data=!3m1!4b1!4m2!3m1!1s0x6bd5f92267e9596d:0x58cd8e35887d45e0?hl=en" target="_blank">
+
+
+    <p>Townsville Civic Theatre :PO Box1006 <br>
+    <a href="https://www.google.com.au/maps/place/Townsville+Civic+Theatre/@-19.271564,146.809208,17z/data=!3m1!4b1!4m2!3m1!1s0x6bd5f92267e9596d:0x58cd8e35887d45e0?hl=en" target="_blank">
     41 Boundary St.
     <br>
     Townsville, QLD 4810</a>
@@ -47,31 +40,25 @@
     Mobile Phone(For non-office hours):0402 255 182
     <br>
     E-mail:<a href="admin@townsvillemusic.org.au" target="_blank"> admin@townsvillemusic.org.au</a></p>
-  <hr>
+    <hr>
     	
-  <h2 class="contact_undertagline"> Thank you to our wonderful sponsors who give us so much support!</h2>
+    <h2 class="contact_undertagline"> Thank you to our wonderful sponsors who give us so much support!</h2>
     <p>Our music concerts are well-publicised and well-attended and sponsors enjoy brand exposure throughout the Arts community. We welcome sponsorship or assistance in any form.</p>
     <hr>
-  <h3> Townsville City Council</h3>
-  <img src="images/TCCcolour150193.gif" alt="towsnsville city" width="71" height="91">
-  <p>The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. 
-  The Council also assists with the performance venues for our concerts and workshops.</p>
-<hr>
-<h3> Queensland Government</h3><img src="images/TGCBF.jpg" width="127" height="91">
-<p> The Gambling Community Benefit Fund has assisted us to obtain office equipment and sound and lighting equipment for our product </p>
-<hr>
-<p class="contact_undertagline"> Like us on Facebook </p>
-<p class="contactfacebook"> <a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534">
-  <img src="images/facebook.gif" alt="facebook icon" ></p></a>
-<<<<<<< Updated upstream
-    
-=======
->>>>>>> Stashed changes
-    
+    <h3> Townsville City Council</h3>
+    <img src="images/TCCcolour150193.gif" alt="towsnsville city" width="71" height="91">
+    <p>The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. 
+    The Council also assists with the performance venues for our concerts and workshops.</p>
+    <hr>
+    <h3> Queensland Government</h3><img src="images/TGCBF.jpg" width="127" height="91">
+    <p> The Gambling Community Benefit Fund has assisted us to obtain office equipment and sound and lighting equipment for our product </p>
+    <hr>
+    <p class="contact_undertagline"> Like us on Facebook </p>
+    <p class="contactfacebook"><a href="https://www.facebook.com/pages/Townsville-Community-Music-Centre/159636880763534">
+    <img src="images/facebook.gif" alt="facebook icon" ></p></a>
                         
-    
+    </div>
 </div>
-
 <!-- End of all Content -->
 
 <!-- Footer Template -->
@@ -79,28 +66,7 @@
 <!-- End Footer Template -->
 
 </body>
-<<<<<<< Updated upstream
 </html>
-=======
-</html>
->>>>>>> Stashed changes
 
-    
-                        
-    
-</div>
-
-<!-- End of all Content -->
-
-<!-- Footer Template -->
-<?php include('template/footer.php'); ?>
-<!-- End Footer Template -->
-
-</body>
-<<<<<<< Updated upstream
-</html>
-=======
-</html>
->>>>>>> Stashed changes
 
 
