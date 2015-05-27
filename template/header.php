a046607345c7e3033e18db4ee7d1cda5f8215146
konk353535 <konk353535@hotmail.com>
2015-05-27 11:43:51 +1000

    Changed userProfile to user_profile to keep conventions

--- template/header.php
+++ template/header.php
@@ -12,7 +12,7 @@
 		<div class="loginForm">
 			<span><?php echo $_SESSION['Name']; ?></span>
 			<form id="form1" name="form1" method="post" action="logout.php?redirect=<?php echo $uri; ?>">
-				<a class="styledLink" href="userProfile.php">My Profile</a>
+				<a class="styledLink" href="user_profile.php">My Profile</a>
 				<div style="text-align:center">
 					<input type="submit" name="logout" value="Log Out" class="input"><br />
 				</div>
